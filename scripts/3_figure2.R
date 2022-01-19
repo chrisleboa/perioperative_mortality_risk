b5ce9d992565922065db07fb56a40644d637ff7c
Chris LeBoa <cleboa@stanford.edu>
2022-01-19 11:14:39 -0800

    increaded plot text size

--- scripts/3_figure2.R
+++ scripts/3_figure2.R
@@ -26,17 +26,23 @@ plot_2 <-
  # geom_text(aes(label = paste(`Percent Reporting`, "%")), vjust = -0.25) +
  # coord_flip() +
   facet_grid(~Group, switch = "y", scales = "free_x", space = "free_x") +
+  theme_minimal() +
   theme(panel.spacing = unit(0, "lines"),
         strip.background = element_blank(),
         strip.placement = "outside",
-        axis.text.x = element_text(angle = 90, vjust = 1, hjust=1),) +
+        axis.text.x = element_text(angle = 90, vjust = 1, hjust=1, size = 12),
+        text = element_text(size = 12),
+        plot.title = element_text(size = 14),
+        legend.position = "none"
+        ) +
   scale_y_continuous(expand=c(0,0)) +
-  # #facet_grid(~Group,
-  #            scales = "free", # Let the x axis vary across facets.
-  #            space = "free" ) + #,  # Let the width of facets vary and force all bars to have the same width.
-            # switch = "both") +
+  facet_grid(~Group,
+             scales = "free", # Let the x axis vary across facets.
+             space = "free" )+ #,  # Let the width of facets vary and force all bars to have the same width.
+           #  switch = "both") +
   labs(
-    title = "Percentage of studies reporting characteristic",
+    y = "Percentage of studies reporting characteristic",
     x = ""
   )
 
+plot_2
